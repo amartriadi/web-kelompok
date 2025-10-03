61a2ea7b6ee73ba235fce9d59f075459a50d2e07
unknown <amartriadi@gmail.com>
2025-10-03 15:24:18 +0700

    upload portfolio project files

---
+++ script.js
@@ -0,0 +1,9 @@
+function donlod() {
+    alert("sedang di download");
+}
+
+const a = document.getElementsByTagName("a");
+const a1 = document.getElementById("About");
+a[0].addEventListener("click", function() {
+     
+});
